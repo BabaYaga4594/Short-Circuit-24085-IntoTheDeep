114710eab241070629d0a3c1a147f1f5780b3804
Vaibhav Pradeep <Vaibhavpradeep777@gmail.com>
2024-10-19 08:08:23 -0400

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrimBotTeleOpFinal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrimBotTeleOpFinal.java
@@ -26,7 +26,7 @@ public class ScrimBotTeleOpFinal extends OpMode {
     final double WRIST_ON_SIDE = 0.8333;
     final double WRIST_EXTENDING_OUT = 0.5;
 
-    int[] maxPositions = {4200}; // potentially 4300
+    int[] maxPositions = {2100}; // potentially something else
 
     int[] minPositions = {0};
 
@@ -38,6 +38,60 @@ public class ScrimBotTeleOpFinal extends OpMode {
         frontRight.setPower((left_y - left_x - right_x) / maxPower);
         backLeft.setPower((left_y - left_x + right_x) / maxPower);
         backRight.setPower((left_y + left_x - right_x) / maxPower);
+    }
+
+    public void moveArmMotor () {
+        int ArmPos = armMotor.getCurrentPosition();
+
+        if(gamepad2.dpad_up) {
+            ArmPos += 50;
+        }
+        else if(gamepad2.dpad_down) {
+            ArmPos -= 50;
+        }
+
+        armMotor.setTargetPosition(ArmPos);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setPower(slideSpeed);
+
+    }
+
+    public void moveArmSlide () {
+        int slidePos = armSlide.getCurrentPosition();
+
+        if(gamepad2.y && slidePos <= maxPositions[0]) {
+            slidePos += 50;
+        }
+        else if(gamepad2.a && slidePos >= minPositions[0]) {
+            slidePos -= 50;
+        }
+
+        armSlide.setTargetPosition(slidePos);
+        armSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armSlide.setPower(slideSpeed);
+    }
+
+    public void moveWristServo() {
+        double servoPosition = wristServo.getPosition();
+        if (gamepad2.left_bumper) {
+            servoPosition = Math.min(1.0, servoPosition + 0.05);
+            wristServo.setPosition(servoPosition);
+        } else if (gamepad2.right_bumper) {
+            servoPosition = Math.max(0.0, servoPosition - 0.05);
+            wristServo.setPosition(servoPosition);
+        }
+    }
+
+    public void moveIntakeServo () {
+        if (gamepad1.a) {
+            intakeServo.setPower(INTAKE_COLLECT);
+        }
+        else if (gamepad1.x) {
+            intakeServo.setPower(INTAKE_OFF);
+        }
+        else if (gamepad1.b) {
+            intakeServo.setPower(INTAKE_DEPOSIT);
+        }
     }
 
     @Override
@@ -53,9 +107,8 @@ public class ScrimBotTeleOpFinal extends OpMode {
         intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
         wristServo = hardwareMap.get(Servo.class, "wristServo");
 
-        armMotor.setTargetPosition(0);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         intakeServo.setPower(INTAKE_OFF);
         wristServo.setPosition(WRIST_ON_SIDE);
@@ -63,6 +116,26 @@ public class ScrimBotTeleOpFinal extends OpMode {
         armSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         armSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
 
         telemetry.addLine("Robot Ready.");
         telemetry.update();
@@ -73,55 +146,10 @@ public class ScrimBotTeleOpFinal extends OpMode {
     public void loop() {
         driveMechanum(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 
-        //intake servo
-        if (gamepad1.a) {
-            intakeServo.setPower(INTAKE_COLLECT);
-        }
-        else if (gamepad1.x) {
-            intakeServo.setPower(INTAKE_OFF);
-        }
-        else if (gamepad1.b) {
-            intakeServo.setPower(INTAKE_DEPOSIT);
-        }
-
-        // wrist servo
-        double servoPosition = wristServo.getPosition();
-        if (gamepad1.x) {
-            servoPosition += 0.05;
-            wristServo.setPosition(servoPosition);
-        }
-        else if (gamepad1.b) {
-            servoPosition -= 0.05;
-            wristServo.setPosition(servoPosition);
-        }
-
-        // arm motor
-        int ArmPos = armMotor.getCurrentPosition();
-
-        if(gamepad2.dpad_up) {
-            ArmPos += 100;
-        }
-        else if(gamepad2.dpad_down) {
-            ArmPos -= 100;
-        }
-
-        armMotor.setTargetPosition(ArmPos);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        armMotor.setPower(slideSpeed);
-
-        // arm slide
-        int slidePos = armSlide.getCurrentPosition();
-
-        if(gamepad2.y && slidePos <= maxPositions[0]) {
-            slidePos += 100;
-        }
-        else if(gamepad2.a && slidePos >= minPositions[0]) {
-            slidePos -= 100;
-        }
-
-        armSlide.setTargetPosition(slidePos);
-        armSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        armSlide.setPower(slideSpeed);
+        moveIntakeServo();
+        moveWristServo();
+        moveArmMotor();
+        moveArmSlide();
 
         telemetry.addData("armTarget: ", armMotor.getTargetPosition());
         telemetry.addData("arm Encoder: ", armMotor.getCurrentPosition());
